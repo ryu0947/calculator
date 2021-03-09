2bb25e6ac0e6b82e7af447339cfb335d8b0fe5b0
ryu <dragonogawaz@gmail.com>
2021-03-10 06:38:52 +0900

    数字パネルを押したら反映される

--- calc.js
+++ calc.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const calc = document.getElementById('calc');
+const numPanels = document.querySelectorAll('.num');
+
+numPanels.forEach( numPanel => {
+    numPanel.addEventListener("click", () => {
+        calc.textContent = numPanel.value;
+    });
+});
