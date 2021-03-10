eae120af958b25d83c8ada4e1b80f5ec5b3b19ac
ryu <dragonogawaz@gmail.com>
2021-03-10 23:43:59 +0900

    10桁までの表示にする

--- calc.js
+++ calc.js
@@ -2,14 +2,17 @@
 
 const calc = document.getElementById("calc");
 const numPanels = document.querySelectorAll(".num");
-let nums = 10;
+let countUp = 1;
 
 numPanels.forEach((numPanel) => {
   numPanel.addEventListener("click", () => {
-    if (calc.textContent === "0") {
-      calc.textContent = numPanel.value;
-    } else {
-      calc.textContent += numPanel.value;
+    if (countUp <= 10) {
+      countUp++;
+      if (calc.textContent === "0") {
+        calc.textContent = numPanel.value;
+      } else {
+        calc.textContent += numPanel.value;
+      }
     }
   });
 });
