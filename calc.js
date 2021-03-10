dbcf1d64ee2560e32eb6c02d037b4124696a7e69
ryu <dragonogawaz@gmail.com>
2021-03-10 21:57:36 +0900

    表示がゼロだったら数字を上書きして、0以外だったら桁を増やしていく

--- calc.js
+++ calc.js
@@ -1,10 +1,15 @@
-'use strict';
+"use strict";
 
-const calc = document.getElementById('calc');
-const numPanels = document.querySelectorAll('.num');
+const calc = document.getElementById("calc");
+const numPanels = document.querySelectorAll(".num");
+let nums = 10;
 
-numPanels.forEach( numPanel => {
-    numPanel.addEventListener("click", () => {
-        calc.textContent = numPanel.value;
-    });
+numPanels.forEach((numPanel) => {
+  numPanel.addEventListener("click", () => {
+    if (calc.textContent === "0") {
+      calc.textContent = numPanel.value;
+    } else {
+      calc.textContent += numPanel.value;
+    }
+  });
 });
