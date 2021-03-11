f52f4a1025e3654fd916d7cc498b9ff618c38f56
ryu <dragonogawaz@gmail.com>
2021-03-11 17:22:15 +0900

    +ボタンを押したか否か判定する変数追加

--- calc.js
+++ calc.js
@@ -6,30 +6,32 @@ const reset = document.getElementById("ac");
 const plus = document.getElementById("plus");
 let countUp = 1;
 let sum = 0;
+let flag = false;
 
 numPanels.forEach((numPanel) => {
   numPanel.addEventListener("click", () => {
-    reset.textContent = "C"; 
+    reset.textContent = "C";
     if (countUp < 10) {
-      if (showNumber.textContent === "0") {
+      if (showNumber.textContent === "0" || flag) {
         showNumber.textContent = numPanel.value;
       } else {
         showNumber.textContent += numPanel.value;
         countUp++;
       }
     } else {
-        showNumber.textContent = "error";
+      showNumber.textContent = "error";
     }
   });
 });
 
 reset.addEventListener("click", () => {
-    showNumber.textContent = "0";
-    reset.textContent = "AC";
-    countUp = 1;
+  showNumber.textContent = "0";
+  reset.textContent = "AC";
+  countUp = 1;
 });
 
-plus.addEventListener('click', () => {
-    sum += Number(showNumber.textContent);
-    plus.classList.toggle('click');
+plus.addEventListener("click", () => {
+  plus.classList.toggle("click");
+  sum += Number(showNumber.textContent);
+  flag = true;
 });
